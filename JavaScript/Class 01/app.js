1da85b380d66891ff5c44a8abcb222cd8c0f5e5e
Meer Quais <meerquais017@gmail.com>
2025-08-08 17:49:57 +0500

    JavaScript Started

---
+++ JavaScript/Class 01/app.js
@@ -0,0 +1,31 @@
+// alert("JavaScript Connected!")
+
+// variable declare kara
+
+// var student = "Mehboob";
+
+
+// initialization // declartion
+
+// var student; = "Bilal";
+
+
+// var student = "Saad Ali";
+// console.log(student);
+// student = "Bilal";
+// console.log(student);
+
+
+// var student = "Bilal";
+
+// var student;
+
+// = "Value"; 
+
+var student = "Bilal";
+
+student = "Minhaj";
+
+
+console.log(student)
+
