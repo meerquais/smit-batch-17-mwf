17ca858018d537d64b0fdfe1a8fc210f4ea04d11
Meer Quais <meerquais017@gmail.com>
2025-08-11 18:00:22 +0500

    Data Types

---
+++ JavaScript/Class 02/app.js
@@ -0,0 +1,106 @@
+// alert("JavaScript is working!");
+
+// console.log("JavaScript is working!");
+
+
+// var std = "Abdul";
+
+
+// console.log(std);
+
+// initializing
+
+// var std;
+// declaring
+// = "Abdul";
+
+
+// var std = "Abdul";
+
+// console.log(std);
+
+// reinitializing
+
+// std = "Ali";
+
+// console.log(std);
+
+
+// legal variable names
+
+// var std = "Abdul"; // legal variable name
+// var std1 = "Ali"; // legal variable name
+// var _std = "Ahmed"; // legal variable name
+// var $std = "Mehboob"; // legal variable name
+
+// Camel Case
+
+// var firstName = "Abdul"; // legal variable name
+// var lastName = "Ali"; // legal variable name
+
+// Snake Case
+
+// var first_name = "Mehboob"; // legal variable name
+// var last_name = "Ahmed"; // legal variable name
+
+// Pascal Case
+
+// var FirstName = "Ali"; // legal variable name
+// var LastName = "Ahmed"; // legal variable name
+
+
+
+
+
+
+// illegal variable names
+
+
+// var 1std = "Student"; // illegal variable name
+
+// var @student = "Student"; // illegal variable name 
+
+// var var = "illegal Variable"; // illegal variable name
+
+
+
+// Primitive Data Types 
+
+// 1. String 
+// 2. Number
+// 3.Boolean
+// 4.Null
+// 5.Undefined
+
+
+// var name = "Ali";
+
+// var userName = 'Ahmed';
+
+// console.log(typeof name)
+
+
+// var age = 26;
+
+// var petrolPrice = 267.89;
+
+// console.log(typeof age);
+
+
+// var isActive = true;
+
+// var isLoggedIn = false;
+
+
+// console.log(typeof isActive);
+// console.log(isLoggedIn);
+
+
+// var result = null;
+
+// console.log(typeof result);
+
+
+var age;
+
+console.log(age);
