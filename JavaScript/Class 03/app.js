cb669a899e39e37c7debc2d90705ad83eab62f19
Meer Quais <meerquais017@gmail.com>
2025-08-13 17:58:04 +0500

    Math Expressions done

---
+++ JavaScript/Class 03/app.js
@@ -0,0 +1,194 @@
+console.log("JavaScript Connected!");
+
+
+// var stdName = "Bilal";
+
+// // declaration
+
+// // var stdName;
+
+
+// stdName = "Saad";
+
+
+// console.log(stdName);
+
+
+//1. String
+//2. Number
+//3. Boolaen
+//4. Null
+//5. Undefined
+
+
+
+// String Data Type
+
+// var str = "This is String!";
+
+// var str1 = 'This is also String!';
+
+
+// console.log(typeof str , str , str1);
+
+
+// Number Data Type
+
+
+// var age = 26 ;
+
+// var price = 268.89;
+
+// console.log(typeof age , typeof price);
+
+
+// Boolean Data Type
+
+
+// var isActive = true ;
+
+// var isLoggedIn = false ;
+
+// console.log(typeof isActive , typeof isLoggedIn);
+
+
+// var result = null ;
+
+// console.log(typeof result);
+
+
+// var stdName = undefined;
+
+
+// console.log(stdName);
+
+
+// var num1 = 10;
+// var num2 = 5;
+
+
+// console.log(num1 + num2);
+// console.log(num1 - num2);
+// console.log(num1 - num2);
+// console.log(num1 * num2);
+// console.log(num1 % num2);
+
+
+// console.log(10 + 5 * 2);
+// console.log((10 + 5) * 2);
+
+
+
+// var popularNumber = 4; 
+
+// var popularNumber = 2 + 2;
+
+// var popularNumber = 12 - 24 ; 
+
+// var popularNumber = 3 * 12 ;
+
+
+// var popularNumber = 10;
+
+// var addition = popularNumber + 20;
+
+// console.log(popularNumber);
+// console.log(addition);
+
+
+// var whatsLeftOf = 10 % 3 ;
+// var whatsLeftOf = 9 % 3 ;
+
+// console.log(whatsLeftOf);
+
+
+// document.write(100 + 200); 
+
+// document.write("<h1>We are creating this from JavaScript!</h1>")
+
+// var num1 = 10;
+// num1++
+
+// // num1 = num1 + 1;
+
+// console.log(num1);
+
+// increment and decrement
+
+
+// pre increment; ++variableName;
+
+// var num = 9;
+// console.log(++num);
+
+
+// // post increment; variableName++ ;
+
+// var num1 = 9;
+
+// console.log(num1++);
+// console.log(num1); 
+
+
+// // pre decrement; --variableName;
+
+// var num2 = 15;
+
+// console.log(--num2);
+
+// // post decrement; variableName-- ;
+
+// var num3 = 15;
+
+// console.log(num3--);
+// console.log(num3);
+
+
+
+// var a = 10;
+
+//     //  9  + 10  - 10  +  8  - 8   + 9 + 9   +  7 + 7 + 7   + 9    - 8 = 
+// var b =  --a + ++a - a-- + --a - a++ + a + a-- + --a + a + a++ + ++a - --a ;
+
+
+//     console.log(b);
+
+
+
+var x = 5;
+//      5   +  7  - 7 - 7 -  6  + 6  + 5    + 4 + 3   +  4 +  4  + 5    + 4  + 5 + 6 = 
+var y = x++ + ++x - x - x - --x + x-- + x-- + x + --x + ++x + x++ + x-- + x++ + x + ++x;
+
+console.log("Value of x is: " + x + " Value of y is " +y);
+
+var a = 3; 
+//      4   + 4   -  4  + 4   + 6   - 6   + 5   + 5   - 6  + 6 =  
+var b = ++a + a++ - --a + a++ + ++a - a-- + a++ + --a - ++a + a++;
+
+console.log(a);
+
+console.log(b);
+
+var m = 7;
+//      7   -  9  + 9   + 7  + 8    - 8   - 8  + 8   + 10 - 10 = 
+var n = m++ - ++m + m-- + --m + ++m - m++ - --m + m++ + ++m - m--;
+
+
+console.log(n);
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
