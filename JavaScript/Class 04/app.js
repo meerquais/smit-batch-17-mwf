a534bebb8e200a317f6819bfa51401c25a8439e9
Meer Quais <meerquais017@gmail.com>
2025-08-15 17:52:44 +0500

    Math Expressions and Prompt Done

---
+++ JavaScript/Class 04/app.js
@@ -0,0 +1,120 @@
+console.log("JavaScript Connected!");
+
+
+// Chapter 1
+
+// Q.1 // A.1
+
+// alert("Welcome to our website!");
+
+// Q.2 // A.2
+
+// alert("Error! Please enter a valid password!");
+
+// Q.3 // A.3
+
+// alert("Welcome to JS Land.. \n Happy Coding!");
+
+// var userName = "Meer Quais";
+
+// document.write("<ul><li>First</li>    </ul>")
+
+// var a = 10;
+// var b = 3;
+
+// var c = a + b * 2;
+
+// console.log(c);
+
+// var a = 10;
+// var b = 3;
+
+// var c = a + 3 ** b;
+
+// console.log(c);
+
+
+// console.log(10 + 20);
+// console.log(50 - 20);
+// console.log(500 / 4);
+// console.log(6 * 5);
+
+// console.log(10 % 3);
+
+
+// var a = 10;
+
+// // var b = a++ ; post increment
+// var b = ++a ;
+
+// console.log(b);
+// console.log(a);
+
+
+// var a = 3;
+
+
+// //      3   + 4   -  2  +  3 +  4   - 3   - 3  +  3   - 3   +  3 = 
+// var b = a++ + a-- - --a + ++a + ++a - --a - a-- + ++a - a++ + --a ;
+
+// console.log(b);
+
+// console.log((3 + 5) * 10);
+
+// var a = 5;
+// var b = "5";
+
+// console.log(a - b );
+
+// var firstName = "Meer";
+// var lastName = "Quais";
+
+// var fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+
+// var StdName = "Meer" ;
+
+// StdName -= " Quais";
+
+// console.log(StdName);
+
+// var a = 10;
+
+// var b = "ten";
+
+// console.log(a - b);
+// console.log(a * b);
+
+
+// var total = 4 + 5;
+
+// console.log("Total = " + total);
+
+// var a = 5;
+
+// var b = "4";
+
+// var c = a - b
+
+// console.log(typeof c , a - b);
+
+
+// var message = "Thanks, ";
+// var userName = "Susan";
+// var banger = "!";
+
+
+// var customMessage = message + userName + banger;
+
+// alert(customMessage)
+
+
+var message = "Thanks, ";
+var fullName = prompt("Type Your Name", "Guest");
+var banger = "!";
+
+var customMessage = message + fullName + banger;
+
+console.log(customMessage);
