caf8778e990251757dd3f0af359eafb6e23f620b
Meer Quais <meerquais017@gmail.com>
2025-08-18 17:56:27 +0500

    If statement

---
+++ JavaScript/Class 05/app.js
@@ -0,0 +1,122 @@
+console.log("JavaScript Connected!");
+
+
+// Chapter 2
+
+// Q.1 // A.1
+
+// var username;
+
+// // Q.2 
+
+// var myName = "Meer Quais";
+
+// // Q.3
+
+// var message;
+
+// message = "Hello World";
+
+// alert(message)
+
+// var msg = "Hello";
+
+// msg = 5;
+
+// alert("Pizza \n Pizz \n Piz \n Pi \n P") 
+
+// document.write("i can write html content thru JavaScript");
+
+// var track = 20;
+
+// alert("You have visited this site " + track + " times!" );
+
+// var num = 5
+// var str = "10";
+
+// console.log(num + str)
+
+
+// var birthYear = 1990;
+
+// document.write("My Birth Year is " + birthYear + "<br/>");
+// document.write("Data type of my declared variable is " + typeof birthYear)
+
+
+// var visitorName = "Abdullah";
+// var productTitle = "T-Shirt";
+// var quantity = 5;
+
+// document.write(visitorName + " ordered " + quantity + " " + productTitle + " on daraz.")
+
+
+
+// var userName = prompt("Type your Full Name" , "Guest" );
+
+// console.log(userName);
+
+//  if statement syntax 
+
+// if(condition){
+//     block of code
+// }else{
+    // block of code
+// }
+
+// var userAge = false;
+
+// if(userAge){
+//     console.log("Allow!")
+// }else{
+//     console.log("Not Allowed!");
+    
+// }
+
+//    > greater than
+//    < less than
+//    >= greater than equal to
+//    <= less than equal to
+
+//     =  assigning operator 
+//     == equality operator
+//    === strictly equality operator\
+//    !== Strictly not quality operator
+//    != not equal to
+
+
+// var userAge = "17";
+
+// if(userAge = 1){
+//     console.log("Adult!");
+// }else{
+//     console.log("Minor");
+    
+// }
+
+
+// var ticket = "4544" ;
+
+// if(ticket != 4544){
+//     alert("Better Luck Next Time");
+// }
+
+
+// if(condition){
+//     block of code
+// }else if(coniditon){
+//     block of code
+// }else{
+//     block of code
+// }
+
+// var marks = prompt("Type your total marks", 70);
+
+// if(marks >= 90){
+//     console.log("Grade A");
+// }else if(marks >=80){
+//     console.log("Grade B");
+// }else if(marks >= 70){
+//     console.log("Grade C");
+// }else{
+//     console.log("You have Failed!");
+// }
