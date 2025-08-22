75eea3adffa2bf22c9fa4b33b44cc40796bb2021
Meer Quais <meerquais017@gmail.com>
2025-08-22 17:54:23 +0500

    If Statements 80% completed

---
+++ JavaScript/Class 06/app.js
@@ -0,0 +1,158 @@
+console.log("JavaScript Conntected!");
+
+
+
+// if(condition){
+//     block of code
+// }
+
+// var age = 18;
+
+
+// if(age >= 18){
+//     console.log("Allowed!");
+// }
+
+
+// var marks = 75;
+
+// if(marks > 50){
+//     console.log("You have passed the exam!");
+// }
+
+
+// var isRaining = true;
+
+// if(isRaining){
+//     console.log("Take an Umbrella");
+// }
+
+// var number = 9;
+
+// if(number % 2 === 0){
+//     console.log("Even number");
+
+// }
+
+// = assigning operator
+// == equal (sirf value check karta hai)
+// === strict equal(value + type dono check karta hai)
+// != not equal 
+// !== strict not equal
+// > greater than
+// < less than
+// >= greater than equal to
+// <= less than equal to
+
+// console.log(5 == "5"); // equal 
+// console.log(5 === "5"); // strict equal
+
+// console.log(10 != "10"); // not equal;
+// console.log(10 !== "10"); // strict not equal 
+
+// console.log(7 > 5); // greater than
+// console.log(3 < 2); // less than 
+// console.log(8 >= 8);
+// console.log(6 <= 5);
+
+
+// var number = prompt("Type a number");
+
+// if(number % 2 === 0){
+//     console.log("This is a even number");
+// }else{
+//     console.log("This is a odd number");
+// }
+
+// if(condition){
+//     block of code jab if true ho.
+// }else if(condition){
+//     block of code jab else if true ho.
+// }else{
+//     block of code jab sare conditions false ho.
+// }
+
+
+// agar marks 90 tho grade A 
+// agar marks 80 tho grade B
+// agar marks 70 tho grade C 
+// warna fail.
+
+
+
+// var marks = prompt("Type your marks");
+
+// if (marks > 100) {
+//     alert("Add marks between 1 to 100;");
+// } else if (marks >= 90) {
+//     console.log("Grade A");
+// } else if (marks >= 80) {
+//     console.log("Grade B");
+// } else if (marks >= 70) {
+//     console.log("Grade C");
+// }
+// else {
+//     console.log("Failed!");
+
+// }
+
+// var num1 = +prompt("Type Number 1");
+// var num2 = +prompt("Type Number 2");
+
+// var opt = prompt("Type your Operator", " '+' '-' '*' '/'");
+
+// if(opt === "+"){
+//     alert(num1 + num2);
+// }else if(opt === "-"){
+//     alert(num1 - num2)
+// }else if(opt === "*"){
+//     alert(num1 * num2)
+// }else if(opt === "/"){
+//     alert(num1 / num2)
+// }else{
+//     alert("Type a valid operator!")
+// }
+
+// OR GATE  AND GATE 
+
+// || <---- OR GATE (Koi ek true hona chahiye)
+// && <---- AND GATE (Dono true hone chahiye)
+
+// var age = 20;
+// var hasID = true;
+
+//     // true        true  == true 
+// if(age >= 18 && hasID){
+//     console.log("You can Vote!");
+// }
+
+// var isWeekend = false;
+// var isFriday = true;
+//     false          true === true; 
+// if(isWeekend || isFriday){
+//     console.log("Condition is True.");
+// }
+
+
+// var age = 25;
+// var hasLicense = true;
+
+// if(age >= 18 && hasLicense){
+//     console.log("You can drive");
+// }else{
+//     console.log("You can't Drive!");
+    
+// }
+
+// var age = 5;
+// var hasLicense = true;
+// var isSober = true;
+
+// if(age >= 18 && hasLicense && isSober){
+//     console.log("You can Drive!");
+// }else{
+//     console.log("you can't drive!");
+    
+// }
+
+
