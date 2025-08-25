bf03d780f68f8d8825e6a035bc57597327f5b0bf
Meer Quais <meerquais017@gmail.com>
2025-08-25 17:48:23 +0500

    If statement Completed! Array started

---
+++ JavaScript/Class 07/app.js
@@ -0,0 +1,189 @@
+console.log("JavaScript Connected!");
+
+
+// var weight = 200;
+
+// var time = 5;
+
+// var age = 18;
+
+// var gender = "Male";
+
+// if(weight > 250 && time < 6 && age > 17 && gender === "Male"){
+//     alert("Come to our tryout!");
+// }else{
+//     alert("Better Luck next time!");
+// }
+
+
+// var SAT = 350;
+// var avg = 600;
+// var gpa = 1.5;
+// var sports = "Football";
+
+// if(SAT > avg || gpa > 2.5 || sports === "Football" ){
+//     alert("Welcome to Uni");
+// }else{
+//     alert("Better luck next time!");
+// }
+
+
+
+// var age = 50;
+
+// var res = "U.S";
+
+// if(age > 65 || (age < 21 && res === "U.S")){
+//     alert("Allow!");
+// }else{
+//     alert("Not Allowed!");
+// }
+
+
+// var total = 500;
+
+// var isVIP = true;
+
+// var coupon = "SALE";
+
+// if(total > 700 && (isVIP === true || coupon === "SALE")){
+//     console.log("20% Discount");
+// }else{
+//     console.log("No Discount!");
+// }
+
+
+// var role = "admin";
+// var emailVerified = true;
+// var isActive = true;
+
+// if(role === "admin" || (emailVerified === false && isActive === false) ){
+//     console.log("Access Granted!");
+    
+// }else{
+//     console.log("Access Denied!");
+    
+// }
+
+
+// var x = 10;
+// var y = 10;
+
+// var a = 20;
+// var b = 10;
+
+// var c = 30;
+// var d = 30;
+
+// var g; 
+
+// var h = "Hello World!";
+
+// var e;
+
+// var f = "This is it!";
+
+// if((x === y || a === b) && c === d){
+//     g = h;
+// }else{
+//     e = f;
+// }
+
+// console.log(g);
+// console.log(e);
+
+// if(c === d){
+
+//     if(x === y){
+//         g = h
+//     }else if(a === b){
+//         g = h;
+//     }else{
+//         e=f
+//     }
+// }else{
+//     e = f
+// }
+// console.log(g);
+// console.log(e);
+
+
+
+// var marks = 90;
+
+// var attendance = 60;
+
+// if(marks > 80 && attendance > 75){
+//     console.log("Passed!");
+// }else{
+//     console.log("Failed!");
+    
+// }
+
+
+// if(marks > 80){
+
+//     if(attendance > 75){
+//         console.log("Passed");
+//     }else{
+//         console.log("Nested IF chala");        
+//         console.log("Failed");
+//     }
+// }else{
+//     console.log("failed!");
+    
+// }
+
+// var email = "abc@gmail.com";
+// var password = "123456";
+
+
+// if(email === "abc@gmail.com" && password === "123456"){
+//     console.log("Login Success");
+// }else{
+//     console.log("Login Failed!");
+    
+// }
+
+
+// var email = "abc@1gmail.com";
+// var password = "123456";
+
+// if(email === "abc@gmail.com"){
+//     if(password === "123456"){
+//         console.log("Login Success");
+//     }else{
+//         console.log("Password Wrong!");
+        
+//     }
+// }else{
+//     console.log("Email incorrect!");
+    
+// }
+
+// var fruit1 = "Apple";
+// var fruit2 = "Mango";
+// var fruit3 = "Banana";
+// var fruti4 = "Peach";
+
+// var fruits = ["Apple" , "Mango" , "Banana" , "Peach" , "Strawberry"];
+
+// console.log(fruit1);
+// console.log(fruit2);
+// console.log(fruit3);
+// console.log(fruti4);
+// console.log(fruits);
+
+
+// var a = [];
+
+// var b = a;
+
+
+// var a = [];
+// var b = [];
+
+// // a = [] == 0.1 = reference 
+// // b = [] == 0.2 = reference 
+
+// console.log(a === b); 
