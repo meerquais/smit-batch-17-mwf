d2e40fc1ee0c067502040d9e086194ce67839e3a
Meer Quais <meerquais017@gmail.com>
2025-08-27 17:46:31 +0500

    Array started

---
+++ JavaScript/Class 08/app.js
@@ -0,0 +1,121 @@
+console.log("JavaScript Connected!");
+
+
+// var fruit1 = "Mango";
+// var fruit2 = "Banana";
+// var fruit3 = "Orange";
+// var fruit4 = "Peach";
+// var fruit5 = "Strawberry";
+
+// console.log(fruit1);
+// console.log(fruit2);
+// console.log(fruit3);
+// console.log(fruit4);
+// console.log(fruit5);
+
+
+// var fruits = ["Mango", "Banana" , "Strawberry" , "Peach" ,"Orange"];
+
+// console.log(fruits);
+// console.log(fruits[2]);
+
+// fruits[1] = "Watermelon";
+
+// console.log(fruits);
+
+
+// console.log("" === "");
+// console.log(1 === 1);
+// console.log(true === true);
+
+// var a = [];
+// var b = [];
+
+// var c = "";
+// var d = "";
+
+// console.log(a === b);
+// console.log(c === d);
+
+
+// var a = [];
+
+// var b = a;
+
+// console.log(a === b );
+
+
+// var cities = ["Karachi" , "Islamabad" , "Lahore" , "Peshawar" , "Quetta"];
+
+// alert("Welcome to " + cities[2]);
+
+// var arr = [1 , "Two" , true , undefined , null , []];
+// console.log(arr);
+
+
+// var pets = [];
+
+
+// pets[0] = "Dogs";
+// pets[1] = "Cats";
+// pets[10] = "Birds"
+
+
+// console.log(pets);
+
+
+// var cities = ["Karachi" , "Islamabad" , "Lahore" , "Peshawar" , "Quetta"];
+
+// console.log(cities);
+
+// cities.pop()
+// console.log(cities);
+
+// var pets =["dog" , "cat" , "bird"];
+// console.log(pets);
+
+// var removed = pets.pop();
+
+// console.log(removed);
+// console.log(pets);
+
+
+// var pets = ["dog" , "cat", "bird" , "snake" ];
+
+// console.log(pets);
+
+// var removed = pets.pop()
+
+// console.log(removed);
+// console.log(pets);
+
+// var fruits = ["Apple" , "Mango" , "Banana" , "Orange"];
+
+// console.log(fruits);
+
+
+// fruits.push("Strawberry");
+
+// console.log(fruits);
+
+// fruits.push("Watermelon" , "Peach" , "Berry");
+
+// console.log(fruits);
+
+
+var fruits = ["Apple" , "Mango" , "Banana" , "Orange"];
+
+console.log(fruits);
+
+// fruits.shift();
+
+// console.log(fruits);
+
+fruits.unshift("Strawberry" , "Peach" , "Blueberry");
+
+console.log(fruits);
+
+
+
+
+
