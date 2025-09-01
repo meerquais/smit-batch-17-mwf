15f78b6f2284b71954d53ed0e28232ce7233abcd
Meer Quais <meerquais017@gmail.com>
2025-09-01 17:41:41 +0500

    Loop Started

---
+++ JavaScript/Class 10/app.js
@@ -0,0 +1,84 @@
+console.log("JavaScript Connected!");
+
+
+
+// var add = (650 * 3) + (100 * 7) + 100
+
+// console.log(add)
+
+
+// var fruits = ["Apple" , "Mango" , "Orange" , "Strawberry"];
+
+// fruits.splice(3,1 , "Pineapple");
+
+// console.log(fruits);
+
+
+// var fruits = ["Apple" , "Mango" , "Orange" , "Strawberry"];
+
+// var copyFruits = fruits.slice(-3);
+
+// console.log(copyFruits);
+// console.log(fruits);
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+
+// for(initialization;condition;incre/decre){
+//     block of code
+// }
+
+// for(var i = 1; i <= 4;i++){
+//     console.log(i);
+    
+// }
+
+// for(var i = 100; i >=1; i--){
+//     console.log(i);
+    
+// }
+
+// infinite loop
+// for(var i = 1; i <=10;){
+//     console.log("Hello World!");
+// }
+
+
+// for(var i = 0; i <= 10; i += 2){
+//     console.log(i);
+    
+// }
+
+
+// var pets = ["dog" , "cat" , "bird" , "snake"];
+
+// for(var i = 0;i < 3;i++){
+//     console.log(pets[i]);
+// }
+
+
+var fruits = ["Apple" , "Mango" , "Banana" , "Orange"];
+
+// console.log(fruits[0]);
+// console.log(fruits[1]);
+// console.log(fruits[2]);
+// console.log(fruits[3]);
+
+
+// for(var i = 0; i < 3; i++){
+//     console.log(fruits[i]);
+    
+// }
+for(var i = 0; i < fruits.length; i++){
+    console.log(fruits[i]);
+    
+}
