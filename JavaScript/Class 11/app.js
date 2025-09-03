6bda8dde8f8168088a75fb11ca6c543f6b61b003
Meer Quais <meerquais017@gmail.com>
2025-09-03 17:46:10 +0500

    Loop Flags

---
+++ JavaScript/Class 11/app.js
@@ -0,0 +1,138 @@
+console.log("JavaScript Connected!");
+
+
+// for(init;condition;incre/decre){
+//     // block of code / for loop ki body 
+// }
+
+
+
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+
+// for(var i = 1;i <= 10;i++){
+//     console.log(i);    
+// }
+
+
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+// var cityToCheck = prompt("Type your City!");
+
+// if(cityToCheck === cleanestCities[0]){
+//     console.log("It's one of the cleanest cities");
+// }else if(cityToCheck === cleanestCities[1]){
+//     console.log("It's one of the cleanest cities");
+// }else if(cityToCheck === cleanestCities[2]){
+//     console.log("It's one of the cleanest cities");
+// }else if(cityToCheck === cleanestCities[3]){
+//     console.log("It's one of the cleanest cities");
+// }else if(cityToCheck === cleanestCities[4]){
+//     console.log("It's one of the cleanest cities");
+// }else{
+//     console.log("It's not on the list!");
+// }
+
+
+// for(var i = 0;i < cleanestCities.length;i++){
+//     if(cityToCheck === cleanestCities[i]){
+//       console.log("It's one of the cleanest cities");
+//     }else{
+//         console.log("It's not on the list!");
+        
+//     }
+// }
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+
+// for(var i = 0;i < cleanestCities.length;i++){
+//     console.log(cleanestCities[i]); 
+// }
+
+// console.log(cleanestCities.length -1);
+
+// for(var i = cleanestCities.length -1; i >= 0;i--){
+//     console.log(i , cleanestCities[i]);
+    
+// }
+
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+
+
+// for(var i = cleanestCities.length -1; i >= 0; i--){
+//     console.log(i , cleanestCities[i]);
+    
+// }
+
+// var nums = [10,20,30,40,45];
+
+// var sum = 0;
+
+// for(var i = 0; i < nums.length;i++){
+
+
+//     sum += nums[i];
+
+//     console.log(i, "i" , nums[i]);
+//     console.log(sum , "Sum");
+    
+    
+//     // console.log(i);
+    
+// }
+
+// console.log(sum);
+
+
+// for(var i = 1; i <= 20;i++){
+//     if(i % 2 === 0){
+//         console.log(i + " is even");
+//     }else{
+//         console.log(i + " is Odd");
+        
+//     }
+// }
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+
+// var cityToCheck = prompt("Type your City");
+
+// for(var i = 0 ; i < cleanestCities.length; i++){
+
+//     if(cityToCheck === cleanestCities[i]){
+//         console.log("This is one of the Cleanest Cities!");
+//     }
+
+// }
+
+
+// var cleanestCities = ["Cheyenne", "Santa Fe", "Tucson", "Great Falls", "Honolulu"];
+
+// var cityToCheck = "Karachi";
+
+// var matchFound = false;
+
+
+// for(var i = 0; i < cleanestCities.length;i++){
+
+
+//     if(cityToCheck === cleanestCities[i]){
+//         matchFound = true
+//         console.log("This is One of the Cleanest Cities");
+//         break;
+//     }
+// }
+
+// if(!matchFound){
+//     console.log("It's not on the list!");
+    
+// }
