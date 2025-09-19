64bde3bb6181bdd9901c964a6a6d0f4b67a896f5
Meer Quais <meerquais017@gmail.com>
2025-09-19 17:52:16 +0500

    String methods

---
+++ JavaScript/Class 16/app.js
@@ -0,0 +1,139 @@
+console.log("JavaScript Connected!");
+
+
+
+
+// console.log("A" === "a");
+
+
+// var cityToCheck = prompt("Enter your city");
+
+
+// var cleanestCities = ["cheyenne" , "santa fe" , "tucson" , "great falls" , "honolulu"];
+
+
+// for(var i = 0;i < cleanestCities.length;i++){
+//     if(cityToCheck === cleanestCities[i]){
+//         console.log("Congo!");        
+//     }
+// }
+
+// var cityToCheck = prompt("Enter your city");
+
+
+
+// if(cityToCheck){
+//     cityToCheck = cityToCheck.trim().toLowerCase()
+// }
+
+
+// var cleanestCities = ["cheyenne" , "santa fe" , "tucson" , "great falls" , "honolulu"];
+
+//  for(var i = 0;i < cleanestCities.length;i++){
+//     if(cityToCheck === cleanestCities[i]){
+//         console.log("Congo!");        
+//     }
+// }
+
+
+// var city = "boston"; // Boston
+// var city = prompt("Type City Name!");
+
+// city.trim();
+
+// var firstChar = city.slice(0,1);
+// var otherChars = city.slice(1);
+
+
+// firstChar = firstChar.toUpperCase();
+// otherChars = otherChars.toLowerCase();
+
+// var capitlizedChar = firstChar + otherChars;
+
+// console.log(capitlizedChar);
+
+// var city = "BOSTON";
+
+// city = city.trim();
+
+// var firstChar = city.slice(0,1);
+// var otherChars = city.slice(1);
+
+// firstChar = firstChar.toUpperCase();
+// otherChars = otherChars.toLowerCase();
+
+// var cappedCity = firstChar + otherChars;
+
+// console.log(cappedCity);
+
+var month = "Doctor";
+
+
+// console.log(month);
+
+
+// var charsInMonth = month.length;
+
+// console.log(charsInMonth);
+
+// var monthAbbrev = month;
+
+// if(charsInMonth > 3) {
+//     monthAbbrev = month.slice(0,3);
+// }
+
+// console.log(monthAbbrev);
+
+var text = `It is startling to think that, even in the darkest depths of World War II, J. R. R. Tolkien was
+writing the trilogy, which contains, with the weird applicability available only to poetry and
+myth, the essential notion that the good gray wizard can understand the evil magi precisely
+because he is just enough like them to grasp their minds and motives in ways that they cannot
+grasp his.`
+
+
+// console.log(text.length);
+
+// // console.log(text.slice(61 , 61 + 12));
+
+
+
+
+// for(var i =0;i < text.length;i++){
+//     if(text.slice(i, i + 12) === "World War II"){
+//         text = text.slice(0,i) + "the Second World War" + text.slice(i + 12);
+//     }
+// }
+
+// console.log(text);
+
+// console.log(text.indexOf("World War II"));
+
+// var index = text.indexOf("World War II");
+
+
+// var firstName = "Ali";
+
+// var firstChar = firstName.charAt(0);
+
+// console.log(firstChar);
+
+
+var name = "Saad";
+
+var lastChar = name.charAt(name.length -1);
+
+console.log(lastChar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
