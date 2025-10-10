92634a78f80caee393d9055955f0138cf0fb06ca
Meer Quais <meerquais017@gmail.com>
2025-10-10 17:57:48 +0500

    Revision from chap 20 to 40

---
+++ JavaScript/Class 25/app.js
@@ -0,0 +1,152 @@
+console.log("JavaScript Connected!");
+
+
+
+// var sentence = "hello world";
+// console.log(sentence.toUpperCase());
+
+// var sentence1 = "HELLO WORLD!";
+
+// console.log(sentence1.toLowerCase());
+
+
+// var word = "meer";
+
+// var capitalized = word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+// console.log(capitalized);
+
+
+// var sentence = "hello";
+
+// console.log(sentence.length);
+
+
+// var s = "JavaScript";
+
+// console.log(s.slice(0,4));
+
+// var s = "JavaScript";
+
+// console.log(s.slice(-5));
+
+// var s = "University"
+
+
+// console.log(s.substring(3,7));
+
+
+
+// var sentence = "JavaScript is a very Powerful Lang";
+
+
+// console.log(sentence.indexOf("Powerful Lang"));
+
+
+// var sentence = "This is an Apple! Apple is good for health!";
+
+// console.log(sentence.indexOf("Apple"));
+// console.log(sentence.lastIndexOf("Apple"));
+
+// var sentence = "JavaScript is very fun!";
+
+// console.log(sentence.charAt(sentence.length -1));
+
+
+// var sentence = "Hello world and world";
+
+// var newSentence = sentence.replace("world" , "Meer");
+
+// console.log(newSentence);
+
+
+// var sentence = "apple apple apple";
+
+// var newSentence = sentence.replaceAll("apple" , "mango");
+
+// console.log(newSentence);
+
+
+
+
+// var sentence = "cog bog mog";
+
+// var newSentence = sentence.replace(/og/g , "at");
+
+// console.log(newSentence);
+
+// var sentence = "Hello hello";
+
+// var newSentence = sentence.replace(/hello/gi , "hi");
+
+// console.log(newSentence);
+
+
+// var num1 = 55.70;
+// var num2 = 55.40;
+
+// console.log(Math.round(num1));
+// console.log(Math.round(num2));
+
+// var num1 = 55.80;
+// var num2 = 55.40;
+
+// console.log(Math.floor(num1));
+// console.log(Math.floor(num2));
+
+
+// var num1 = 55.80;
+// var num2 = 55.40;
+
+// console.log(Math.ceil(num1));
+// console.log(Math.ceil(num2));
+
+// var random = Math.floor(Math.random() * 5 + 1);
+
+// console.log(random);
+
+
+// var input = +prompt("Type a number");
+
+// var StrToNum = parseInt(input);
+// var StrToNumWithDeci = parseFloat(input);
+// var strWithNum = Number(input);
+
+
+// console.log(input);
+// console.log(typeof input);
+// console.log(StrToNum);
+// console.log(typeof StrToNum);
+// console.log(StrToNumWithDeci);
+// console.log(typeof StrToNumWithDeci);
+// console.log(strWithNum);
+// console.log(typeof strWithNum);
+
+// var num = 594.55;
+
+// var numToStr = num.toString();
+// var numToString = String(num)
+// console.log(numToStr);
+// console.log(numToString);
+
+
+
+// var input = prompt();
+
+// var convertToNum = Number(input);
+
+
+
+// if(isNaN(input)){
+//     alert("Please do not put anything other than the number!")
+// }
+
+// console.log(convertToNum);
+
+// var num = 59.4949494;
+
+// console.log(num.toFixed(2));
+
+
+var later = new Date("July 2, 2028");
+
+console.log(later);
