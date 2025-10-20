46ac0b37d75ceeb831f0dec1c0eb20e095dd4547
Meer Quais <meerquais017@gmail.com>
2025-10-20 17:54:19 +0500

    Events Started

---
+++ JavaScript/Class 27/app.js
@@ -0,0 +1,19 @@
+// this is a single line comment!
+
+/* this
+    is 
+    a
+    multi
+    line
+    comment */
+
+
+function foo(name){
+    // alert("You have Clicked the Link using function!");
+    alert("Hello " + name)
+}
+
+
+function imgChange(){
+    
+}
