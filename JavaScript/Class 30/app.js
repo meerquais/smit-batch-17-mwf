c7fce206d9fafa40c9f82b8b46578efb4563db37
Meer Quais <meerquais017@gmail.com>
2025-10-27 17:50:30 +0500

    Dom Started

---
+++ JavaScript/Class 30/app.js
@@ -0,0 +1,98 @@
+console.log("JavaScript Connected!");
+
+
+// function showMore(){
+
+//     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deserunt quibusdam et, deleniti voluptate sequi? Sed laboriosam hic corrupti dignissimos commodi eius error unde ipsam. Possimus ratione quia voluptatibus quae.Debitis impedit voluptate reprehenderit odit quos delectus, quidem at aspernatur nesciunt. Repudiandae, eveniet rem unde incidunt magnam veritatis modi a ab fugit quibusdam possimus debitis sed repellendus ullam alias atque."
+
+
+//     document.getElementById("para").innerText = text
+
+// }
+
+
+// function showMore(){
+
+//     var text = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet deserunt quibusdam et, deleniti voluptate sequi? Sed laboriosam hic corrupti dignissimos commodi eius error unde ipsam. Possimus ratione quia voluptatibus quae.Debitis impedit voluptate reprehenderit odit quos delectus, quidem at aspernatur nesciunt. Repudiandae, eveniet rem unde incidunt magnam veritatis modi a ab fugit quibusdam possimus debitis sed repellendus ullam alias atque."
+
+//     document.getElementById("para").innerHTML = text + "<a href='javascript:void(0)' onclick='showLess()'>Show less..</a>"
+
+
+// }
+
+// function showLess(){
+
+
+//     var text = "Lorem";
+
+//     document.getElementById("para").innerHTML = text + "<a href='javascript:void(0)' onclick='showMore()'>Show more</a>"
+
+// }
+
+// function toggleText(){
+//     var moreText = document.getElementById("moreText");
+//     var btn = document.getElementById("toggleBtn");
+    
+    
+
+//     if(moreText.style.display === "none"){
+//         moreText.style.display = "block"
+//         btn.textContent = "Show less";
+//     }else{
+//         moreText.style.display = "none"
+//         btn.textContent = "Show More"
+//     }
+
+// }
+
+// function makeInvisible(){
+
+//     document.getElementById("ugly").className += " hidden"
+// }
+
+
+// function swapPic(){
+//     document.getElementById("before").src = "./assets/on.png"
+// }
+
+
+// function swapPic(eleId, newPic){
+
+//     document.getElementById(eleId).src = newPic;
+
+// }
+
+// function makeBig(){
+//     document.getElementById("p1").className += " big"
+// }
+
+
+// function makeBig(){
+//     // document.getElementById("p1").style.fontSize = "32px"
+
+//     var ele = document.getElementById("p1");
+
+//     ele.style.fontSize = "32px"
+//     ele.style.color = "red"
+// }
+
+// var margin = document.getElementById("mainPic").style.margin ;
+
+// console.log(margin);
+
+
+// var paras  = document.getElementsByTagName("p");
+
+
+// var text = "This text was changed!";
+
+// paras[0].innerText  = text
+
+// for(var i = 0; i < paras.length;i++){
+//     paras[i].style.fontFamily = "Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif";
+// }
+
+// console.log(paras);
+
+
+console.log(document);
