c6ab881cf02aa76c425fe810d65fec98f492c32f
Meer Quais <meerquais017@gmail.com>
2025-12-05 17:43:55 +0500

    Browser Control

---
+++ Advance JavaScript/Class 07/app.js
@@ -0,0 +1,30 @@
+console.log("Js Connected!");
+
+
+// let whereAt = window.location.href;
+
+// console.log(whereAt);
+
+// let theDomain = window.location.hostname;
+
+// console.log(theDomain);
+
+
+// let thePath = window.location.pathname;
+
+// console.log(thePath);
+
+
+// let theAnchor = window.location.hash;
+
+// console.log(theAnchor);
+
+
+function GoToLocation(){
+    // window.location.href = "https://www.google.com/"
+    // window.location.assign("https://www.google.com/");
+    // window.location.replace("https://www.youtube.com/");
+
+    window.location.reload();
+
+}
