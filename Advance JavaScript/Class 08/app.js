286b55752f2b708f4da41e201a1d479c49798c6e
Meer Quais <meerquais017@gmail.com>
2025-12-26 17:49:33 +0500

    Advance JavaScript

--- Advance JavaScript/Class 08/app.js
+++ Advance JavaScript/Class 08/app.js
@@ -147,11 +147,200 @@ console.log("JavaScript Connected!");
 // console.log(prod);
 
 
-const letters = ["a" , "b" , "a", "c" , "b" ];
+// const letters = ["a" , "b" , "a", "c" , "b" ];
 
-const freq = letters.reduce((acc,ch)=>{
-    acc[ch] = (acc[ch] || 0) + 1;
-    return acc
-}, {})
+// const freq = letters.reduce((acc,ch)=>{
+//     acc[ch] = (acc[ch] || 0) + 1;
+//     return acc
+// }, {})
 
-console.log(freq);
+// console.log(freq);
+
+// find 
+
+// const arr = [1,3,5,9];
+
+// const firstEven = arr.find((num)=>{
+//     return num % 2 === 0;
+// })
+
+// console.log(firstEven);
+
+// const users = [
+//     {id:1},
+//     {id:2},
+//     {id:3,
+//         name:"Hafeez"
+
+//     },
+//     {id:4},
+//     {
+//         id:3,
+//         name:"Meer"
+//     },
+// ]
+
+// const user = users.find((u)=>{
+//     return u.id === 3;
+// })
+
+// console.log(user);
+
+
+// findIndex
+
+// const arr = [2,3,4,6,7];
+
+// console.log(arr.findIndex((n)=>{
+//     return n % 2 === 1
+// }));
+
+
+// const users = [
+//     {id:1},
+//     {id:2},
+//     {id:4},
+// ]
+
+// console.log(users.findIndex((u)=>{
+//     return u.id === 3;
+// }));
+
+
+// const arr = [1,2,3];
+
+// const idx = arr.findIndex((x)=>{
+//     return x === 2;
+// });
+
+// if(idx !== -1){
+//     arr.splice(idx , 1);
+// };
+
+// console.log(arr);
+
+
+// console.log([1,3,5].some((n)=>{
+//     return n % 2 === 0;
+// }));
+
+// console.log([1,4,5].some((n)=>{
+//     return n % 2 === 0;
+// }));
+
+// const users = [
+//     {active:false},
+//     {active:false}
+// ]
+
+// console.log(users.some((u)=>{
+//     return u.active;}));
+
+
+
+// const arr = [2,3,6]
+
+// console.log(arr.every((n)=>{
+//     return n % 2 === 0
+// }));
+
+// console.log(arr.every((n)=>{
+//     return n % 2 === 0
+// }));
+
+
+// const users = [
+//     {verfied:true},
+//     {verfied:false}
+// ]
+
+// console.log(users.every((u)=>{
+//     return u.verfied
+// }));
+
+
+
+// const arr = [10,20,30];
+
+// let a = arr[0]
+// let b = arr[1]
+// let c = arr[2]
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// const [a , b , c] = arr
+
+// console.log(a , b , c);
+
+
+// const arr = [1,2,3,4];
+
+// const [x , ,z] = arr;
+
+// console.log(x, z);
+
+// const arr = [];
+
+// const [ a = 1 , b = 2] = arr;
+
+// console.log(a , b );
+
+
+
+// let p = 1 
+
+// let q = 2;
+
+
+// [p , q] = [q , p];
+
+// console.log(p , q);
+
+// const nested = [1, [2,3] , 4];
+
+// const [one , [two , three] ,four] = nested;
+
+// console.log(one , two , three , four);
+
+
+// const user = {
+//     name: "Ali",
+//     age:25
+// };
+
+// const {name , age} = user;
+
+// console.log(name , age);
+
+// const user = {
+//     name : "ahrar",
+//     city: "Karachi"
+// }
+
+// const {name = "guest" , city = "Unknown" } = user;
+
+// console.log(name , city);
+
+
+// const obj = {
+//     id:1,
+//     title:"Book"
+// }
+
+// const {title: bookTitle , id:bookId} = obj;
+
+// console.log(bookTitle , bookId);
+
+
+const student = {
+    name:"Ali",
+    age:25,
+    course:"MWA"
+};
+
+const {name: studentName , age: studentAge , course: studentCourse} = student;
+
+
+console.log(studentName , studentAge , studentCourse);
