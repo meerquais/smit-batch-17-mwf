8f7e665d8e0c444341b4a6be10a16ce25a18505c
Meer Quais <meerquais017@gmail.com>
2026-01-02 17:51:46 +0500

    call , apply , bind , OOP

--- Advance JavaScript/Class 10/app.js
+++ Advance JavaScript/Class 10/app.js
@@ -195,3 +195,284 @@
 
 
 // console.log(result);
+
+
+// call ==> arg ko alag alag receive karta hai.
+
+
+
+
+
+// function say(){
+//     console.log(this.name);    
+// }
+
+// say()
+// const obj = {name:"Meer"};
+// say.call(obj)
+
+// const name = "Meer";
+// const age = 26;
+// const position = "Trainer";
+
+
+// console.log(name);
+
+
+// function say(){
+//     console.log(this.name);
+// }
+
+// const obj = {
+//     name:"Meer",
+//     age:26,
+//     position:"Trainer"
+// }
+
+// say.call(obj);
+
+
+// function greet(greeting, punctuation){
+//     console.log(greeting + ", " + this.name + punctuation);
+// }
+
+// const user = {
+//     name: "Ali"
+// }
+
+// greet.call(user, "Hello" , "!");
+
+
+
+// const obj1 = {
+//     value:10,
+//     getValue: function(){
+//         return this.value;
+//     }
+// }
+
+// const obj2 = {
+//     value:42
+// }
+
+
+// console.log(obj1.getValue.call(obj2));
+// console.log(obj1.getValue());
+
+// function showLength(){
+//     console.log(this.length);
+// }
+
+// const obj = {0:"a" , 1:"b", 2:"c" , length:3};
+
+// showLength.call(obj)
+
+
+// function Person(name){
+//     this.name = name;
+// }
+
+// function createWithCall(name){
+//     const obj = {};
+//     Person.call(obj, name);
+//     return obj;
+// }
+
+// console.log(createWithCall("Meer"));
+
+
+// apply ==> arguments array ke form mein diye jate hai.
+
+
+// function sum(a , b , c ){
+//     return a + b + c;
+// }
+
+// // console.log(sum(20 , 30 , 40));
+
+
+// const arr = [4,6,8];
+
+// // console.log(sum(arr[0] , arr[1] , arr[2]));
+
+// console.log(sum.apply(null,arr));
+
+
+
+
+// const numbers = [5,6,2,9];
+
+// console.log(Math.max.apply(null, numbers));
+
+
+// function Person(name,age){
+//     this.name = name;
+//     this.age = age
+// }
+
+// function create(name,age){
+//     const obj = {};
+//     Person.apply(obj, [name,age]);
+//     return obj
+// }
+
+// console.log(create("Meer", 26));
+
+
+
+// Bind ===> bind function ko call nahi karta, balki ek naya function return karta hai.
+
+
+// function sayHello(){
+//     console.log("hello ," + this.name);    
+// }
+
+// const person = {
+//     name:"Ahrar"
+// };
+
+// const bound = sayHello.bind(person)
+
+// bound()
+
+
+
+// function outer(){
+//     var message = "hello";
+//     function inner(){
+//         console.log(message);
+//     }
+//     return inner;
+// }
+
+// var fn = outer();
+
+// fn()
+
+// function makeCounter(){
+//     let count = 0;
+
+//     return function(){
+//         count += 1;
+//         return count;
+//     }
+// }
+
+
+// let c1 = makeCounter();
+
+// console.log(c1());
+// console.log(c1());
+// console.log(c1());
+// console.log(c1());
+// console.log(c1());
+
+// let c2 = makeCounter();
+
+// console.log(c2());
+// console.log(c2());
+
+// getter // setter 
+
+// function createPerson(name){
+//     let _name = name // private 
+
+//     return {
+//         getName: function(){
+//             return _name;
+//         },
+//         setName: function(newName){
+//             _name = newName;
+//         }
+//     };
+
+// };
+
+// let p = createPerson("Meer");
+
+
+// console.log(p.getName());
+// console.log(p._name);
+
+
+// p.setName("Ali");
+
+// console.log(p.getName());
+
+
+
+
+// let person = {
+//     name:"Ali",
+//     age:22,
+//     greet:function(){
+//         console.log("Hello, i am " + this.name);        
+//     }
+// };
+
+// person.greet();
+
+// function Car(model , year){
+//     this.model = model;
+//     this.year = year;
+//     this.info = function(){
+//         console.log("Model: " + this.model + ", Year: " + this.year);        
+//     }
+// }
+
+// let car1 = new Car("Civic", 2024);
+
+// car1.info();
+
+
+// function Animal(name){
+//     this.name = name;
+// }
+
+// Animal.prototype.speak = function(){
+//     console.log(this.name + " makes a sound");    
+// }
+
+// function Dog(name){
+//     Animal.call(this,name)
+// }
+
+// Dog.prototype = Object.create(Animal.prototype);
+
+// let d = new Dog("Rex");
+
+// d.speak();
+
+// class Student {
+
+//     constructor(name,roll){
+//         this.name = name;
+//         this.roll = roll
+//     }
+
+//     show(){
+//         console.log(this.name + " - " + this.roll);        
+//     }
+// }
+
+// let student1 = new Student("Ahrar" , 13);
+
+// student1.show()
+
+// class Animal {
+//     constructor(name) {
+//         this.name = name;
+//     }
+//     speak(){
+//         console.log(this.name + " makes a sound");
+//     }
+
+// }
+// class Cat extends Animal {
+//     speak(){
+//         console.log(this.name + " Meows");        
+//     }
+// }
+
+// let cat = new Cat("Kitty");
+// cat.speak();
