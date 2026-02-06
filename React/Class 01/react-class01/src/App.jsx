b9ca392a22fb68efd9c80a3c2145bc66e8482db2
Meer Quais <meerquais017@gmail.com>
2026-02-06 17:54:56 +0500

    React Started

---
+++ React/Class 01/react-class01/src/App.jsx
@@ -0,0 +1,25 @@
+import Hello from "./Components/Hello"
+import Welcome from "./Components/Welcome"
+
+
+
+function App() {
+  
+
+  
+  
+
+  return (
+   <>
+   
+
+    <Hello />
+    
+    <Welcome />
+   
+   
+   </>
+  )
+}
+
+export default App
---
+++ React/Class 01/react-class01/src/Components/Hello.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Hello.css"
+
+function Hello() {
+  return (
+    <div>
+      <h1 className='color'>Hello There React!</h1>
+    </div>
+  )
+}
+
+export default Hello
